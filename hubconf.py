08b0a8cb00500907dda29faad71b593846694cfa
Kranthi Sedamaki <skranthi4444@gmail.com>
2022-09-12 10:21:33 +0530

    update train

--- hubconf.py
+++ hubconf.py
@@ -132,12 +132,13 @@ def _test(dataloader, model, loss_fn=loss_fn):
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
     
-def train(train_dataloader, test_dataloader, epochs=5):
+def train(train_dataloader, test_dataloader, model1, epochs=5):
     for t in range(epochs):
         print(f"Epoch {t+1}\n-------------------------------")
-        _train(train_dataloader, model, loss_fn, optimizer)
-        _test(test_dataloader, model, loss_fn)
+        _train(train_dataloader, model1, loss_fn, optimizer)
+        _test(test_dataloader, model1, loss_fn)
     print("Done!")
+    return model1
 
 def save_model(mypath="model.pth"):
     torch.save(model.state_dict(), "model.pth")
